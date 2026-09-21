83e9a103c7fa35a4efa30da55a540217b5850db6
Yuri Victorovich <yuri@FreeBSD.org>
2026-09-20 20:24:00 -0700

    libtorrent-rasterbar-2.1.1 compatibility

--- src/torrent/torrentcontext.cpp
+++ src/torrent/torrentcontext.cpp
@@ -912,14 +912,14 @@ bool TorrentContext::_addTorrent(Torrent *torrent) // resumeTorrent
         // QByteArray bytes = source.toUtf8();
 
         auto ptr = bytes.constData();
-        auto size = static_cast<boost::string_view::size_type>(bytes.size());
+	auto size = static_cast<std::string_view::size_type>(bytes.size());
 
         lt::string_view uri { ptr, size };
         lt::error_code ec;
-        p = lt::parse_magnet_uri(uri.to_string(), ec);
+	p = lt::parse_magnet_uri(std::string(uri), ec);
         if (ec) {
             qDebug_1 << "invalid magnet link:";
-            qDebug_1 << QString::fromStdString(uri.to_string());
+	     qDebug_1 << QString::fromUtf8(uri.data(), uri.size());
             qDebug_1 << QString::fromStdString(ec.message());
             return false;
         }
--- src/torrent/utils.cpp
+++ src/torrent/utils.cpp
@@ -16,7 +16,6 @@
 #include "utils.h"
 
 #include "libtorrent/add_torrent_params.hpp"
-#include "libtorrent/hex.hpp"               // to_hex, from_hex
 #include "libtorrent/magnet_uri.hpp"
 #include "libtorrent/torrent_info.hpp"
 
@@ -24,21 +23,19 @@
 UniqueId TorrentUtils::toUniqueId(const lt::sha1_hash &hash)
 {
     if (!hash.is_all_zeros()) {
-        auto hex = lt::aux::to_hex(hash);
-        return QString::fromStdString(hex).toUpper();
+        return QString::fromLatin1(QByteArray(hash.data(), hash.size()).toHex()).toUpper();
     }
     return {};
 }
 
 lt::sha1_hash TorrentUtils::fromUniqueId(const UniqueId &uuid)
 {
-    lt::span<char const> in(uuid.toStdString());
-    lt::sha1_hash out;
-    if (!lt::aux::from_hex(in, out.data())) {
+    const QByteArray bin = QByteArray::fromHex(uuid.toLatin1());
+    if (bin.size() != lt::sha1_hash::size()) {
         qWarning() << "invalid info-hash";
         return lt::sha1_hash();
     }
-    return out;
+    return lt::sha1_hash(lt::span<char const>(bin.constData(), bin.size()));
 }
 
 /******************************************************************************
@@ -375,14 +372,13 @@ QString TorrentUtils::toString(const std::string &s)
 
 QString TorrentUtils::toString(const lt::string_view &s)
 {
-    return QString::fromStdString(s.to_string()).toUpper();
+    return QString::fromUtf8(s.data(), s.size()).toUpper();
 }
 
 QString TorrentUtils::toString(const lt::sha1_hash &hash)
 {
     if (!hash.is_all_zeros()) {
-        auto hex = lt::aux::to_hex(hash);
-        return QString::fromStdString(hex).toUpper();
+        return QString::fromLatin1(QByteArray(hash.data(), hash.size()).toHex()).toUpper();
     }
     return {};
 }
